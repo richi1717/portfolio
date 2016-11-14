3711100a88d654bdb379944edb8465427de803db
Richi Jeffery <kikki_sawn@yahoo.com>
2016-11-13 22:22:07 -0700

    add references to the references page

--- src/js/components/References.jsx
+++ src/js/components/References.jsx
@@ -10,6 +10,9 @@ class References extends Component {
       <div className="references">
         <h1>References</h1>
         <ul>
+          <li>Herb Adams |&nbsp;
+            <a href="mailto:hadams2@yahoo.com?Subject=Job%20Reference:%20Richi%20Jeffery" target="_top">hadams2@yahoo.com</a>
+          </li>
           <li>Todd Kurtz |&nbsp;
             <a target="_blank" href="tel:602-478-5884">602.478.5884</a> |&nbsp;
             <a href="mailto:toddalankurtz@yahoo.com?Subject=Job%20Reference:%20Richi%20Jeffery" target="_top">toddalankurtz@yahoo.com</a>
@@ -18,11 +21,18 @@ class References extends Component {
             <a target="_blank" href="tel:602-750-1386">602.750.1386</a> |&nbsp;
             <a href="mailto:brantstuns@gmail.com?Subject=Job%20Reference:%20Richi%20Jeffery" target="_top">brantstuns@gmail.com</a>
           </li>
+          <li>Kevin Lanni |&nbsp;
+            <a target="_blank" href="tel:424-243-6406">424.243.6406</a> |&nbsp;
+            <a href="mailto:therealklanni@gmail.com?Subject=Job%20Reference:%20Richi%20Jeffery" target="_top">therealklanni@gmail.com</a>
+          </li>
+          <li>Jon Hirsch |&nbsp;
+            <a target="_blank" href="tel:760-419-2881">760.419.2881</a> |&nbsp;
+            <a href="mailto:jon.hirsch@csaa.com?Subject=Job%20Reference:%20Richi%20Jeffery" target="_top">jon.hirsch@csaa.com</a>
+          </li>
         </ul>
       </div>
     );
   }
-
 
   handleClick() {
     const CARD = this.props.selected === 'references' ? null : 'references';
