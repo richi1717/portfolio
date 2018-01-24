738bf8dc589ad6d030db8de3bd1516d63fd2ac3f
Richi Jeffery <rici1717@gmail.com>
2018-01-24 02:22:09 -0700

    fix more stuff

--- webpack.cf.config.js
+++ webpack.cf.config.js
@@ -1,20 +1,39 @@
-var config = require('./webpack.config.js');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
 
-config.plugins.push(
-  new webpack.DefinePlugin({
-    "process.env": {
-      "NODE_ENV": JSON.stringify("production")
-    }
-  })
-);
-
-config.plugins.push(
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: false
-    }
-  })
-);
-
-module.exports = config;
+module.exports = {
+  entry: ['whatwg-fetch', './client/js/index.js'],
+  devtool: 'cheap-module-source-map',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  resolve: {
+    modules: ['client/js', 'node_modules']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [path.join(__dirname, 'client/js/')],
+        exclude: /node_modules/,
+        options: { cacheDirectory: true }
+      },
+      {
+        test: /\.s?css$/,
+        loaders: ['style-loader', 'css-loader?sourceMap?root=.', 'sass-loader?outputStyle=compressed&sourceMap']
+      },
+      {
+        test: /\.(svg|png|jpe?g)$/,
+        loader: 'url-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'production'
+    })
+  ],
+};
