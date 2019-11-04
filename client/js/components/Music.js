7994deb2788775704e8c5d09e48e53fe05ec6edb
Richi Jeffery <rici1717@gmail.com>
2019-11-04 11:50:33 -0700

    add poppyNotEet and ruby songs

--- client/js/components/Music.js
+++ client/js/components/Music.js
@@ -1,72 +1,36 @@
 import React, { useState } from 'react';
+import mobileCheck from '../helpers/mobileCheck';
+import musicList from '../constants/musicList';
 
 const Music = () => {
   const [selected, setSelected] = useState('');
 
   const renderSelectedAudio = () => (
     <div className="audio-container">
-      {musicSelection.find(music => music.src === selected).name}
+      {musicList.find(music => music.src === selected).name}
       <audio src={selected} controls autoPlay>
         Your browser does not support the <code>audio</code> element.
       </audio>
     </div>
   );
 
-  const musicSelection = [
-    {
-      name: 'Britty - Incomplete',
-      src: '/brittyIncomplete.mp3',
-      original: true
-    },
-    { name: 'E-Intro', src: '/eIntro.mp3', original: true },
-    {
-      name: 'The First song I ever wrote digitally',
-      src: '/richiFirst.mp3',
-      original: true
-    },
-    { name: 'Song of Storms', src: '/songOfStorms.mp3', original: false },
-    {
-      name: 'Legend of Zelda - Dungeon',
-      src: '/zeldaDungeon.mp3',
-      original: false
-    },
-    {
-      name: 'Final Fantasy VII - Still More Fighting',
-      src: '/ffviiStillMoreFighting.mp3',
-      original: false
-    },
-    {
-      name: 'Megalomania work in progress',
-      src: '/megalomaniaWIP.mp3',
-      original: false
-    },
-    {
-      name: 'Pokémon work in progress',
-      src: '/pokemonBackgroundWIP.mp3',
-      original: false
-    },
-    { name: 'Final Fantasy VII - Jenova', src: '/jenova.mp3', original: false },
-    {
-      name: 'Sonic - Green Hill Zone',
-      src: '/greenhillZoneSonic.mp3',
-      original: false
-    }
-  ];
+  const originalMusic = musicList.filter(music => music.original);
+  const notOriginalMusic = musicList.filter(music => !music.original);
 
-  const originalMusic = musicSelection.filter(music => music.original);
-  const notOriginalMusic = musicSelection.filter(music => !music.original);
   const handleClick = src => () => {
     setSelected(src);
-    window.scrollTo(0, window.innerHeight);
+    if (mobileCheck()) {
+      window.scrollTo(0, window.innerHeight);
+    }
   };
 
   return (
     <div className="music-page-container">
       <p>
-        I enjoy writing music in my free time, here are some of the songs I've
+        I enjoy writing music in my free time. Here are some of the songs I've
         written. Keep in mind some of these are very much a work in progress.
-        The first few are ones I wrote, the second section are from video games
-        that I thought would be fun to make as well.
+        The first section are ones that I wrote, the second section are from
+        video games that I thought would be fun to make as well.
       </p>
       <div className="music-container">
         <div className="music-button-wrapper">
---
+++ client/js/constants/musicList.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    name: 'Ruby',
+    src: '/ruby.m4a',
+    original: true
+  },
+  {
+    name: 'Britty - Incomplete',
+    src: '/brittyIncomplete.mp3',
+    original: true
+  },
+  {
+    name: 'Poppy not Eet',
+    src: '/poppyNotEet.m4a',
+    original: true
+  },
+  { name: 'E-Intro', src: '/eIntro.mp3', original: true },
+  {
+    name: 'The First song I ever wrote digitally',
+    src: '/richiFirst.mp3',
+    original: true
+  },
+  { name: 'Song of Storms', src: '/songOfStorms.mp3', original: false },
+  {
+    name: 'Legend of Zelda - Dungeon',
+    src: '/zeldaDungeon.mp3',
+    original: false
+  },
+  {
+    name: 'Final Fantasy VII - Still More Fighting',
+    src: '/ffviiStillMoreFighting.mp3',
+    original: false
+  },
+  {
+    name: 'Megalomania work in progress',
+    src: '/megalomaniaWIP.mp3',
+    original: false
+  },
+  {
+    name: 'Pokémon work in progress',
+    src: '/pokemonBackgroundWIP.mp3',
+    original: false
+  },
+  { name: 'Final Fantasy VII - Jenova', src: '/jenova.mp3', original: false },
+  {
+    name: 'Sonic - Green Hill Zone',
+    src: '/greenhillZoneSonic.mp3',
+    original: false
+  }
+];
